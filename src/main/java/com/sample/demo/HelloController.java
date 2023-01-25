ec64d64ceae795101f65771d1e455152cf3a733e
juwonLee <rreone2017@gmail.com>
2023-01-25 17:31:41 +0900

    feat: request 값 추가

--- src/main/java/com/sample/demo/HelloController.java
+++ src/main/java/com/sample/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home() {
         return "hello world";
     }
